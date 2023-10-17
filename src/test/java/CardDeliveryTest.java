8d8acee06a628021e0e59a529f9bc6cf3ebc07e4
Sona <s.harutyunyan1992@gmail.com>
2023-10-17 14:38:29 +0400

    Positive path

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,32 +1,41 @@
-import com.codeborne.selenide.SelenideElement;
+
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Configuration.timeout;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
 
 public class CardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
-    public void shouldTestPositive() {
+    public void shouldBePositivePath() {
+
+
         open("http://localhost:9999/");
-        SelenideElement form=$(By.className("form_theme_alfa-on-white"));
+       //SelenideElement form = $(By.className("form_theme_alfa-on-white"));
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").setValue("22.10.2023");
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys (Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name]input").setValue("Федор Иванов");
         $("[data-test-id=phone]input").setValue("+71236549874");
-        $("[data-test-id=agreement]input").click();
+        $("[data-test-id=agreement]").click();
         $("button").click();
-        //$("[data-test-id=notification]").shouldHave(exactText("Встреча успешно забронирована на "));
-        $(withText("Встреча успешно забронирована на ")).shouldBe(visible, Duration.ofSeconds(10));git g
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+
+        ;
 
     }
 }
